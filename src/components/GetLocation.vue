9f0f20a8cd004af29d454ac3f275b1462aa5d5d7
zeyadsleem <zeyadsleem1@gmail.com>
2024-01-26 04:07:46 +0200

    format: getLocation

--- src/components/GetLocation.vue
+++ src/components/GetLocation.vue
@@ -5,30 +5,30 @@ import type { Ref } from "vue";
 import WeatherReport from "./WeatherReport.vue";
 
 type Geolocation = {
-  latitude: number;
-  longitude: number;
+    latitude: number;
+    longitude: number;
 };
 
 const coords: Ref<Geolocation | undefined> = ref();
 const geolocationBlockedByUser: Ref<boolean> = ref(false);
 
 const getGeolocation = async (): Promise<void> => {
-  await navigator?.geolocation?.getCurrentPosition(
-    async (position: { coords: Geolocation }) => {
-      coords.value = position.coords;
-    },
-    (error: { message: string }) => {
-      geolocationBlockedByUser.value = true;
-      console.error(error.message);
-    }
-  );
+    await navigator?.geolocation?.getCurrentPosition(
+        async (position: { coords: Geolocation }) => {
+            coords.value = position.coords;
+        },
+        (error: { message: string }) => {
+            geolocationBlockedByUser.value = true;
+            console.error(error.message);
+        }
+    );
 };
 
 onMounted(async () => {
-  await getGeolocation();
+    await getGeolocation();
 });
 </script>
 <template>
-  <WeatherReport v-if="coords && !geolocationBlockedByUser" :coords="coords" />
-  <div v-if="geolocationBlockedByUser">User denied access</div>
+    <WeatherReport v-if="coords && !geolocationBlockedByUser" :coords="coords" />
+    <div v-if="geolocationBlockedByUser">User denied access</div>
 </template>
